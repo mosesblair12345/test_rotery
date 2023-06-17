0d1de9ec82f531c03148cc9380fc89d71ed74d28
moses <mosesblair00@gmail.com>
2023-06-17 12:21:49 +0300

    run

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const port = 3000
 const app= express();
+
 app.use(express.json());
+
 app.post("/rotary",(req,res)=>{
     const {
-    sessionId,
-    serviceCode,
-    phoneNumber,
-    text
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        text
     } = req.body;
+
     let response = '';
 
     if (text == '') {
-        response = `CON Welcome to Rotary district 9212
-
-        To register enter your Full Name`;
-
-        res.set('Content-Type: text/plain');
-        res.send(response)
+        response = `Welcome to Rotary district 9212`;
     }
+    res.set('Content-Type: text/plain');
+    res.send(response)
 })
 app.listen(port,()=>{
     console.log(`i am listening on port ${port}`)
